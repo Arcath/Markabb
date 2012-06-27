250332e1effe68180cc7588064cf127dec35be22
Arcath <adam@arcath.net>
2012-06-27 20:01:23 +0100

    add nicer table tags, closes #2

--- spec/tags/table_spec.rb
+++ spec/tags/table_spec.rb
@@ -4,8 +4,8 @@ describe "Table Tags" do
     include MarkabbMacros::Formatting
     
     it_should_apply_formatting ['table_header', 'th'],
-        ['table_row', 'tr'],
-        ['table_cell', 'td']
+        ['table_tr', 'tr'],
+        ['table_td', 'td']
         
     it "should draw a table with a width" do
         Markabb.parse("[table]table[/table]").should eq("<table width=\"100%\">table</table>")
@@ -15,5 +15,13 @@ describe "Table Tags" do
         Markabb.parse "[table]table[/table]" do |c|
             c.table_width = "75%"
         end.should eq("<table width=\"75%\">table</table>")
+    end
+    
+    it "should work for [row]" do
+        "[row]..[/row]".markabb.should eq "<tr>..</tr>"
+    end
+    
+    it "should work for [cell]" do
+        "[cell]..[/cell]".markabb.should eq "<td>..</td>"
     end
 end
