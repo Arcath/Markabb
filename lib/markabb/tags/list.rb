e77b66281c4cfa834c8cc01f71a5a92d7436ddb6
Arcath <adam@arcath.net>
2012-07-16 10:32:49 +0100

    change smart lists to simple tag

--- lib/markabb/tags/list.rb
+++ lib/markabb/tags/list.rb
@@ -13,4 +13,4 @@ list_callback = Markabb::Callback.new(Proc.new { |s|
 Markabb.register_tag :un_ordered, Markabb::Tag.new('ul', '<ul>\1</ul>'), :lists
 Markabb.register_tag :ordered, Markabb::Tag.new('ol', '<ol>\1</ol>'), :lists
 Markabb.register_tag :list_item, Markabb::Tag.new('li', '<li>\1</li>'), :lists
-Markabb.register_tag :smart_list, Markabb::Tag.new(/\[list\](.*?)\[\/list\]/, list_callback), :lists
+Markabb.register_tag :smart_list, Markabb::Tag.new('list', list_callback), :lists
